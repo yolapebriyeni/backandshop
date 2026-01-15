7d8a9d260c3c58233458ab895b434e1aa54d3226
yolapebriyeni <pebriyeniyola@gmail.com>
2026-01-15 08:26:48 +0700

    fix: use internal railway environment variables

--- dbconnect.php
+++ dbconnect.php
@@ -1,21 +1,11 @@
 <?php
 
-
-
 define('DB_HOST', 'mainline.proxy.rlwy.net');
-
 define('DB_USER', 'root');
-
-define('DB_PASS', 'nQwUQMMHxZkAdZciZwivmELPizwbvYU');
-
+define('DB_PASS', 'nQwUQMMHxZkAdZciZwivmELPizwbvYUh');
 define('DB_NAME', 'railway');
-
 define('DB_PORT', 17049);
-
-
 
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT);
 
-
-
-?>, 
+?>
